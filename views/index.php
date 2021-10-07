3c9ae20cac49df184dfef87935c8768819432bb4
Aurum1611 <neeyatlotlikar@gmail.com>
2021-10-07 11:10:36 +0530

    Added php and made more partials

---
+++ views/index.php
@@ -0,0 +1,3 @@
+<?php echo file_get_contents("partials/nav-bar.html"); ?> 
+<?php echo file_get_contents("partials/repeat-content.html"); ?> 
+<?php echo file_get_contents("partials/footer.html"); ?>
