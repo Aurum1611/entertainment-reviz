db036b7b8a9f65ebf15e389d37b076d6f6dd5092
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-01-03 17:19:46 +0530

    Fixed database and init

--- db.config.php
+++ db.config.php
@@ -1,27 +1,38 @@
 <?php
 
-$fileCreds = fopen("credentials.json", "r") or die("Unable to open file!");
-$dbCred = json_decode(fread($fileCreds, filesize("credentials.json")));
-fclose($fileCreds);
+$servername = "localhost";
+$username = "root";
+$password = "";
+
 
 // Create and Connect
-$conn = new mysqli($dbCreds->servername, $dbCreds->username, $dbCreds->password);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } // else echo "Connected successfully";
 
-$sql = "CREATE DATABASE entertain; USE entertain;";
-$conn->query($sql);
+function runQuery($conn, $sql)
+{
+    try {
+        return $conn->query($sql);
+    } catch (Exception $e) {
+        echo '<script>alert(' . $e->getMessage() . ');</script>';
+    }
+}
 
-$sql = "create table Users (userid int not null auto_increment primary key,
-username varchar(255) not null, password varchar(255) not null, email varchar(255) not null);";
-$conn->query($sql);
+runQuery($conn, 'CREATE DATABASE entertain;');
+runQuery($conn, 'USE entertain;');
 
-$sql = "create table Bookmarks (bmno int auto_increment, uid int not null, uname varchar(255) not null, 
-postid varchar(255) not null);";
-$conn->query($sql);
+
+$sql = "create table Users (userid int not null primary key auto_increment,
+            username varchar(255) not null, password varchar(255) not null, email varchar(255) not null);";
+runQuery($conn, $sql);
+
+$sql = "create table Bookmarks (srno int primary key auto_increment, uid int not null, 
+            uname varchar(255) not null, postid varchar(255) not null);";
+runQuery($conn, $sql);
 
 
 // Controller
@@ -30,33 +41,33 @@ $conn->query($sql);
 function insertUser($conn, $userid, $username, $password, $email)
 {
     $sql = "INSERT INTO Users (userid, username, password, email) VALUES ($userid, $username, $password, $email)";
-    return $conn->query($sql);
+    return runQuery($conn, $sql);
 }
 
 function updateUser($conn, $userid, $username, $password, $email)
 {
     $sql = "UPDATE Users password = $password WHERE username = $username AND email = $email;";
-    return $conn->query($sql);
+    return runQuery($conn, $sql);
 }
 
 function findUser($conn, $userid, $username, $password, $email)
 {
     $sql = "SELECT * FROM Users WHERE userid = $userid AND username = $username AND password = $password
             AND email = $email;";
-    $result = $conn->query($sql);
+    $result = runQuery($conn, $sql);
     return $result;
 }
 
 function insertBookmark($conn, $uid, $uname, $postid)
 {
     $sql = "INSERT INTO Bookmarks (uid, uname, postid) VALUES ($uid, $uname, $postid);";
-    return $conn->query($sql);
+    return runQuery($conn, $sql);
 }
 
 function findBookmark($conn, $uid, $uname, $postid)
 {
     $sql = "SELECT * FROM Bookmarks WHERE uid = $uid AND uname = $uname;";
-    $result = $conn->query($sql);
+    $result = runQuery($conn, $sql);
     return $result;
 }
 
@@ -66,27 +77,27 @@ function findBookmark($conn, $uid, $uname, $postid)
 function viewUser($conn, $userid, $username, $email)
 {
     $sql = "SELECT * FROM Users WHERE userid = $userid AND username = $username AND email = $email;";
-    $result = $conn->query($sql);
+    $result = runQuery($conn, $sql);
     return $result;
 }
 
 function deleteUser($conn, $username, $email, $password)
 {
     $sql = "DELETE FROM Users WHERE username = $username AND email = $email AND password = $password;";
-    return $conn->query($sql);
+    return runQuery($conn, $sql);
 }
 
 function viewBookmarks($conn, $uid, $uname)
 {
     $sql = "SELECT * FROM Bookmarks WHERE uid = $uid AND uname = $uname;";
-    $result = $conn->query($sql);
+    $result = runQuery($conn, $sql);
     return $result;
 }
 
 function deleteBookmark($conn, $uid, $uname, $postid)
 {
     $sql = "DELETE FROM Bookmarks WHERE uid = $uid AND uname = $uname AND postid = $postid;";
-    return $conn->query($sql);
+    return runQuery($conn, $sql);
 }
 
 $conn->close();
